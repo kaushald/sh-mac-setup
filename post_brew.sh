5fce198c8211cbf331e90ea160ecb10ae309bd27
Kaushal Dalvi <kaushaldalvi@gmail.com>
2022-07-30 17:58:20 -0400

    brew installs

--- post_brew.sh
+++ post_brew.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 echo "Brew installing..."
+brew update
 brew tap charmbracelet/tap && brew install charmbracelet/tap/gum
 echo "Installed"
 echo "============================================================"
 
 clear 
 
-gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Hello, there! Welcome to $(gum style --foreground 212 'Kaos Mac Setup')."
+gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Welcome to $(gum style --foreground 212 'Kaos Mac Setup')."
 echo -e "Can you tell me a $(gum style --italic --foreground 99 'your full name')?\n"
 NAME=$(gum input --placeholder "What is your full name?")
 
@@ -27,7 +28,7 @@ gum spin -s monkey --title "Saving email..." -- sleep 2 && clear
 # SSH
 #############################################################################################
 
-gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Hello, there! Welcome to $(gum style --foreground 212 'SSH Setup')."
+gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Starting $(gum style --foreground 212 'SSH Setup')."
 
 gum spin --spinner monkey --title "Deleting existing SSH keys..." -- rm -rf ~/.ssh/
 
@@ -51,7 +52,7 @@ gum confirm "Are you Done?" && echo "Great! Proceeding..."  && sleep 1 && clear 
 # XCODE
 #############################################################################################
 
-gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Hello, there! Welcome to $(gum style --foreground 212 'XCODE Setup')."
+gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Starting $(gum style --foreground 212 'XCODE Setup')."
 
 echo -e "Follow the $(gum style --italic --foreground 99 'prompts')!\n"
 
@@ -65,17 +66,54 @@ clear
 # GIT
 #############################################################################################
 
-gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Hello, there! Welcome to $(gum style --foreground 212 'GIT Setup')."
-
-
-# Update homebrew recipes
-echo "Updating homebrew..."
-brew update
-
-echo "Installing Git..."
-brew install git
+gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Starting $(gum style --foreground 212 'GIT Setup')."
 
 echo "Git config"
 
-git config --global user.name "Kaushal Dalvi"
-git config --global user.email kaushaldalvi@gmail.com
+git config --global user.name "$NAME"
+git config --global user.email "$EMAIL"
+
+#############################################################################################
+# BREW
+#############################################################################################
+
+gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Starting $(gum style --foreground 212 'BREW Installs')."
+
+for TOOL in fish gh exa gradle sqlite fzf k9s pipx fd kubernetes-cli bat
+do
+    gum spin -s monkey --title " Installing $TOOL..." -- brew install $TOOL
+    echo -e "\n\n :pager: Installed $TOOL \n\n" | gum format -t emoji
+done
+
+#############################################################################################
+# CASKS
+#############################################################################################
+
+gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Staring $(gum style --foreground 212 'CASKS Installs')."
+
+
+#############################################################################################
+# FISH
+#############################################################################################
+
+gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Staring $(gum style --foreground 212 'FISH Setup')."
+
+gum spin -s monkey --title " Installing fisher..." -- curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+echo -e "\n\n :pager: Installed fisher \n\n" | gum format -t emoji
+
+
+gum spin -s monkey --title " Installing fisher..." -- fish_add_path /opt/homebrew/bin
+echo -e "\n\n :computer: Added brew to fish path \n\n" | gum format -t emoji
+
+
+echo -e "\n\n :computer: Switch to fish and install plugins :computer:" | gum format -t emoji
+gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "$(gum style --foreground 212 "\
+"'fisher install jorgebucaran/nvm.fish'"\
+" 'fisher install IlanCosman/tide@v5'"\
+" 'fisher install PatrickF1/fzf.fish'"\
+" 'fzf_configure_bindings --help'"\
+" 'fisher install andreiborisov/sponge'"\
+" 'fisher install jethrokuan/z'"\
+" 'pipx install dunk'"\
+" 'fisher install jorgebucaran/autopair.fish')"
+
--- trial.sh
+++ trial.sh
@@ -2,54 +2,19 @@
 
 clear 
 
-gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Hello, there! Welcome to $(gum style --foreground 212 'Kaos Mac Setup')."
-echo -e "Can you tell me a $(gum style --italic --foreground 99 'your full name')?\n"
-NAME=$(gum input --placeholder "What is your full name?")
-
-echo -e "Well, it is nice to meet you, $(gum style --foreground 212 "$NAME").\n\n"
-
-gum spin -s pulse --title "Saving name..." -- sleep 2
-
-echo -e "Can you tell me your $(gum style --italic --foreground 99 'email address')?\n"
-
-EMAIL=$(gum input --placeholder "What is your email?")
-
-gum spin -s monkey --title "Saving email..." -- sleep 2
-
-echo -e "Will use this email to configure git $(gum style --foreground 212 "$EMAIL").\n\n"
-
-clear; echo "What should I do with this information?"; sleep 1
-
 READ="Read"; THINK="Think"; DISCARD="Discard"
 ACTIONS=$(gum choose --cursor-prefix "[ ] " --selected-prefix "[✓] " --no-limit "$READ" "$THINK" "$DISCARD")
 
 clear; echo "One moment, please."
 
-grep -q "$READ" <<< "$ACTIONS" && gum spin -s line --title "Reading the secret..." -- sleep 1
-grep -q "$THINK" <<< "$ACTIONS" && gum spin -s pulse --title "Thinking about your secret..." -- sleep 1
-grep -q "$DISCARD" <<< "$ACTIONS" && gum spin -s monkey --title " Discarding your secret..." -- sleep 2
+grep -q "$FISH" <<< "$ACTIONS" && gum spin -s line --title "Reading the secret..." -- sleep 1
+grep -q "$GH" <<< "$ACTIONS" && gum spin -s pulse --title "Thinking about your secret..." -- sleep 1
+grep -q "$EXA" <<< "$ACTIONS" && gum spin -s monkey --title " Discarding your secret..." -- sleep 1
+
+
+
+fish
 
 sleep 1; clear
 
-echo "What's your favorite $(gum style --foreground 212 "Gum") flavor?"
-GUM=$(echo -e "Cherry\nGrape\nLime\nOrange" | gum filter)
-echo "I'll keep that in mind!"
-
-sleep 1; clear
-
-echo "Do you like $(gum style --foreground "#04B575" "Bubble Gum?")"
-sleep 1
-
-CHOICE=$(gum choose --item.foreground 250 "Yes" "No" "It's complicated")
-
-[[ "$CHOICE" == "Yes" ]] && echo "I thought so, $(gum style --bold "Bubble Gum") is the best." || echo "I'm sorry to hear that."
-
-sleep 1
-
-gum spin --title "Chewing some $(gum style --foreground "#04B575" "$GUM") bubble gum..." -- sleep 5
-
-clear
-
-NICE_MEETING_YOU=$(gum style --height 5 --width 25 --padding '1 3' --border double --border-foreground 57 "Well, it was nice meeting you, $(gum style --foreground 212 "$NAME"). Hope to see you soon!")
-CHEW_BUBBLE_GUM=$(gum style --width 25 --padding '1 3' --border double --border-foreground 212 "Don't forget to chew some $(gum style --foreground "#04B575" "$GUM") bubble gum.")
-gum join --horizontal "$NICE_MEETING_YOU" "$CHEW_BUBBLE_GUM"
+echo "And now we are in fish."
